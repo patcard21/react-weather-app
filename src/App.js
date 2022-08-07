7d2c2a2e9621b42c6ccc8cd0bd384e505770c258
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-07 17:23:41 -0400

    Clean up + added link to GH repo in footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Weather App</h1>
+      </header>
+      <footer>
+        {" "}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/patcard21"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Coded by Patricia Cardona
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
