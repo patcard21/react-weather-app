98078213e280fe8e927442985602464402a32f37
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-08 17:46:32 -0400

    Creating the skeleton of the weather app in Weather.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from "./Weather.js";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather />
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,26 @@
 import React from "react";
 import "./Weather.css";
+
 export default function Weather() {
-  return <div>Hello from Weather.js</div>;
+  return (
+    <div className="Weather">
+      Hello from Weather.js
+      <form>
+        <div className="row">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            className="form-control"
+          />
+          <input type="button" value="Search" className="btn btn-primary" />
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wed 7:00pm</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      
+    </div>
+  );
 }
