1e330a36cbdccc2c725044f50f9525fbd06127e9
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-08 16:14:54 -0400

    Issues on an npm install. More details in descr.
    --Problem fixed-- Was experiencing errors on npm install. Since I had not yet added much code, I saved what I had, deleted all files in my folder and reinstalled.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
-      <footer>
-        {" "}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
-          href="https://github.com/patcard21"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Coded by Patricia Cardona
+          Learn React
         </a>
-      </footer>
+      </header>
     </div>
   );
 }
