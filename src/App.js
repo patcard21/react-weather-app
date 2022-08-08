1cad56d034430fc30ca2d92ebd07f47c3252a0b3
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-08 16:56:02 -0400

    Created new Weather component with css file too

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
+        <Weather />
       </header>
       <footer>
         {" "}
