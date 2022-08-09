538614553eb2814e79b4b58df22d041fabb51867
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-09 15:20:02 -0400

    Formatted for live data

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     console.log(response.data);
@@ -13,7 +17,7 @@ export default function Weather(props) {
       coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "Wednesday 07:00",
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       iconUrl: "https://ssl.gstatic/onebox/weather/64/partly_cloudy.png",
       wind: response.data.wind.speed,
@@ -30,45 +34,34 @@ export default function Weather(props) {
   }
   function search() {
     const apiKey = "f4e257f61e44c9e6b5b1129ee526efb9";
-    let city = "New York";
     let apiUrl = ` https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        Hello from Weather.js
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
                 type="search"
                 placeholder="Enter a city.."
                 className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
-              <input type="button" value="Search" className="btn btn-primary" />
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
         </form>
-        <h1>New York</h1>
-        <ul>
-          <li>Wed 7:00pm</li>
-          <li>Mostly Cloudy</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <h3>{Math.round(temperature)}</h3>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 15%</li>
-              <li>Humidity: 72%</li>
-              <li>Wind: 13km/ph</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
